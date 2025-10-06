10a7f3ec1853d89527cce7cb5efba32a2367ec76
Roberto Estrada <redoneplumbing@Robertos-iMac.local>
2025-10-06 15:16:12 -0500

    feat(core): wrap app with NativeBase, React Query, and AuthProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Slot } from "expo-router";
 import { NativeBaseProvider } from "native-base";
 import React from "react";
+import { AuthProvider } from "../lib/auth";
 
 const queryClient = new QueryClient();
 
@@ -10,7 +11,9 @@ export default function RootLayout() {
   return (
     <NativeBaseProvider>
       <QueryClientProvider client={queryClient}>
-        <Slot />
+        <AuthProvider>
+          <Slot />
+        </AuthProvider>
       </QueryClientProvider>
     </NativeBaseProvider>
   );
